6b370b56138854fa3c7e3f4a8d2ae163b5e554b4
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2020-11-16 12:31:53 +0100

    added functions

--- script.js
+++ script.js
@@ -10,11 +10,44 @@ function addEventListeners() {
 
 function fetchDays(event) {
     $.ajax({
-        url: "http://api.dryg.net/dagar/v2.1/1989",
+        url: "http://api.dryg.net/dagar/v2.1/2020",
         type: "GET",
         dataType: "jsonp",
-        complete: function(response) {
-            console.log(response)
+        success: function(response) {
+            presentHolidays(response.dagar);
         }
-    })
+    });
+}
+
+function presentHolidays(daysInAYear) {
+    const container = document.getElementById("holidays-container");
+    container.innerHTML = "";
+
+    const holidays = getHolidays(daysInAYear);
+    const listItems = createListItems(holidays);
+    container.append(...listItems);
+}
+
+function createListItems(holidays) {
+    const listItems = [];
+
+    for (const day of holidays) {
+        const li = document.createElement("li");
+        li.innerHTML = day.helgdag + " = " + day.datum + " " + day.veckodag;
+        listItems.push(li);
+    }
+
+    return listItems;
+}
+
+function getHolidays(daysInAYear) {
+    const holidays = [];
+
+    for(const day of daysInAYear) {
+        if(day.helgdag) {
+            holidays.push(day);
+        }
+    }
+
+    return holidays;
 }
